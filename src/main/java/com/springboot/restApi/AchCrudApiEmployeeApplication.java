8296c41fea55df4893811375ac75e8317d44cb2f
VIJAY <vijayrathore727@gmail.com>
2023-10-09 16:08:50 +0530

    Basic architecture of Project...

---
+++ src/main/java/com/springboot/restApi/AchCrudApiEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.restApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AchCrudApiEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AchCrudApiEmployeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/restApi/AchCrudApiEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.restApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AchCrudApiEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
